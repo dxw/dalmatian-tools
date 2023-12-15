d76cb5edee4dbf1753618c79bb41d69407ab1b80
Chris Wright <chris@dxw.com>
2023-12-15 12:52:32 +0000

    Use AWS time when comparing AWS timestamps
    * When comparing timestamps from AWS, we should use the time from the
      AWS NTP servers, as this is more likely to be the source of the
      timestamps used within AWS - Rather than using the local time which
      can differ on different machines.

---
+++ lib/bash-functions/aws_epoch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Returns the epoch in seconds from AWS NTP.
+# This should be the most reliable current timestamp when comparing timestamps
+# from AWS resources, rather than using the date/time from a local machine.
+#
+# @usage aws_epoch
+function aws_epoch {
+  AWS_NTP_SERVER="0.amazon.pool.ntp.org"
+
+  NTP_STRING="$(sntp "$AWS_NTP_SERVER" | tail -n1)"
+  AWS_DATE="$(echo "$NTP_STRING" | cut -d ' ' -f 1)"
+  AWS_TIME="$(echo "$NTP_STRING" | cut -d ' ' -f 2 | cut -d '.' -f 1)"
+  AWS_TZ="$(echo "$NTP_STRING" | cut -d ' ' -f 3)"
+
+  AWS_EPOCH="$(date -j -f "%Y-%m-%d %T (%z)" "+%s" "$AWS_DATE $AWS_TIME $AWS_TZ")"
+  echo "$AWS_EPOCH"
+}
