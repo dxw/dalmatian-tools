4f1f5aad569e533eea796f669dc4954822fe9d74
Chris Wright <chris@dxw.com>
2024-03-20 16:44:48 +0000

    Allow external AWS accounts
    * This allows external (Non SSO Org accounts) to be initialised,
      bootstrapped and infrastructure deployed.
    * When initialising an external account, it prompts the user to manually
      add a Role in the External account, with the required Trust
      Relationship provided. The Role name will be
      `<main-dalmatian-account-id>-dalmatian-access`.
    * When generating the configuration, a profile will be added that uses
      the main dalmatian profile as a source profile, and the role arn of
      the role created in the External account. This creats an
      authentication flow of SSO Login -> Dalmatian Main -> External Account

---
+++ lib/bash-functions/append_sso_config_file_assume_role.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Dalmatian specific function
+# Appends a profile which assumes a role to the
+# configuration file
+function append_sso_config_file_assume_role {
+  config_file="$1"
+  profile_name="$2"
+  source_profile="$3"
+  role_arn="$4"
+
+  cat <<EOT >> "$config_file"
+[profile $profile_name]
+source_profile = $source_profile
+role_arn = $role_arn
+
+EOT
+}
