71d41309304f661f693d4ab09f4921db997c1b8b
Chris Wright <chris@dxw.com>
2024-05-20 17:57:09 +0100

    Fix: Add region to external account config
    * The region is required in the aws configuration

--- lib/bash-functions/append_sso_config_file_assume_role.sh
+++ lib/bash-functions/append_sso_config_file_assume_role.sh
@@ -10,11 +10,13 @@ function append_sso_config_file_assume_role {
   profile_name="$2"
   source_profile="$3"
   role_arn="$4"
+  region="$5"
 
   cat <<EOT >> "$config_file"
 [profile $profile_name]
 source_profile = $source_profile
 role_arn = $role_arn
+region = $region
 
 EOT
 }
