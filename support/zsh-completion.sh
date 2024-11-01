3d19baf6d547cedec01ec6d81250e61d6d3c0448
Sarah Mount <sarah.mount@dxw.com>
2024-11-01 17:37:39 +0000

    Zsh tab completion works for Dalmatian v1

--- support/zsh-completion.sh
+++ support/zsh-completion.sh
@@ -28,51 +28,51 @@ _dalmatian_completions() {
 
   case "$compline" in
     'aurora'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/aurora" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/aurora/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'certificate'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/certificate" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/certificate/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'cloudfront'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/cloudfront" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/cloudfront/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'service'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/service" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/service/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'config')
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/config" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/config/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'util'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/util" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/util/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'rds'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/rds" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/rds/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'ecs'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/ecs" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/ecs/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'waf'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/waf" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/waf/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'aws'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/aws" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/aws/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'ci'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/ci" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/ci/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     'elasticache'*)
-      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/elasticache" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
+      while read -r; do COMPREPLY+=( "$REPLY" ); done < <( compgen -W "$(_dalmatian_completions_filter "$(find "$bindir/elasticache/v1" -type f -mindepth 1 -maxdepth 1 -exec basename {} \;)")" -- "$cur" )
       ;;
 
     *)
